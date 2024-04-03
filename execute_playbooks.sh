cadf9a9f914fd6a576028d2324b526a5c1a86466
kriricha <90825381+kriricha@users.noreply.github.com>
2024-04-03 14:43:53 +0530

    Update execute_playbooks.sh

--- execute_playbooks.sh
+++ execute_playbooks.sh
@@ -1,13 +1,21 @@
-#!/bin/bash
+/bin/bash
 
-# Execute elastic playbook
-ansible-playbook elastic.yml
-
-# Execute mykibana playbook
-ansible-playbook mykibana.yml
-
-# Execute myfilebeat playbook
-ansible-playbook myfilebeat.yml
-
-# Execute logstash playbook
-ansible-playbook mylogstash.yml
+choice="$1"
+case $choice in
+        elasticsearch)
+                echo "elastic search runnng"
+                ansible-playbook ./ansible/elastic.yml
+                ;;
+        kibana) echo "you select kibana"
+                ansible-playbook ./ansible/mykibana.yml
+                ;;
+        filebeat) echo "you select filebeat"
+                ansible-playbook ./ansible/myfilebeat.yml
+                ;;
+        logstash) echo "you select logstash"
+                ansible-playbook ./ansible/mylogstash.yml
+                ;;
+        quit) echo "Quitting" exit
+                ;;
+        *) echo "default selection";;
+esac
